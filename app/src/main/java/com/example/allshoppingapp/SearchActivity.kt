53d829bbe5fd0176faa147e2e31f6202cd4d6e8a
kaito <16t2803a@shinshu-u.ac.jp>
2020-04-13 10:33:29 +0900

    画面遷移とUIの作成

--- app/src/main/java/com/example/allshoppingapp/SearchActivity.kt
+++ app/src/main/java/com/example/allshoppingapp/SearchActivity.kt
@@ -3,16 +3,17 @@ package com.example.allshoppingapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_search.*
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
-class MainActivity : AppCompatActivity() {
-
-
+class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_search)
 
-        searchButton.setOnClickListener {
+        searchButton.setOnClickListener{
+            finish()
             val intent = Intent(application, SearchActivity::class.java)
             startActivity(intent)
         }
